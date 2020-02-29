965bdca9ea7ab26543f78aa8741b260260fc970d
Bilal El Houdaigui <bilal.elhoudaigui@gmail.com>
2020-02-29 15:54:58 +0100

    added book and publisher to the Model and View

--- mrt_app/app/models.py
+++ mrt_app/app/models.py
@@ -1,6 +1,6 @@
 from flask_appbuilder import Model
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Text, Boolean, Date
+from sqlalchemy.orm import relationship, backref
 
 """
 
@@ -10,3 +10,41 @@ AuditMixin will add automatic timestamp of created and modified by who
 
 
 """
+
+
+class Publisher(Model):
+    __tablename__ = 'publisher'
+
+    publisher_id = Column(Integer, primary_key=True, autoincrement=True)
+    publisher_name = Column(String(60), nullable=False)
+
+    def __repr__(self):
+        return self.publisher_name
+
+class Book(Model):
+    __tablename__ = 'book'
+
+    book_id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    book_isbn = Column(String(255), nullable=False, unique=True)
+    book_cover = Column(String(255), nullable=False)
+    book_title = Column(String(255), nullable=False)
+    book_pages = Column(Integer)
+    book_summary = Column(Text)
+    book_avg_rating = Column(Float)
+    book_publication_date = Column(Date)
+    book_is_approved = Column(Boolean, default=False)
+    publisher_id = Column(
+        Integer,
+        ForeignKey(
+            # _constraint='books_publisher_id_fk',
+            # column='publisher.publisher_id',
+            'publisher.publisher_id'
+            # ondelete='RESTRICT',
+            # onupdate='RESTRICT'
+        )
+    )
+    publisher = relationship("Publisher")
+
+    def __repr__(self):
+        return self.book_title
+
--- mrt_app/app/views.py
+++ mrt_app/app/views.py
@@ -1,111 +1,45 @@
-from flask import render_template
+from flask_appbuilder import ModelView
 from flask_appbuilder.models.sqla.interface import SQLAInterface
-from flask_appbuilder import ModelView, ModelRestApi, has_access
 
-from . import appbuilder, db
-
-from flask_appbuilder import AppBuilder, expose, BaseView
-
-from wtforms import Form, StringField
-from wtforms.validators import DataRequired
-from flask_appbuilder.fieldwidgets import BS3TextFieldWidget
-from flask_appbuilder.forms import DynamicForm
-from flask import flash
-from flask_appbuilder import SimpleFormView
-from flask_babel import lazy_gettext as _
+from app import appbuilder, db
+from app.models import Publisher, Book
 
 
-class ContactUsForm(DynamicForm):
-    title = StringField('Title',
-                        description="Reason why you're contacting us!",
-                        validators=[DataRequired()], widget=BS3TextFieldWidget())
-    description = StringField('Description',
-                              description='Your message...', widget=BS3TextFieldWidget())
+class BookModelView(ModelView):
+    datamodel = SQLAInterface(Book)
+    label_columns = {'publisher': 'Publisher'}
+    list_columns = [
+        'book_title', 'book_summary', 'book_avg_rating',
+        'book_is_approved', 'publisher']
 
-
-class ContactUsFormView(SimpleFormView):
-    form = ContactUsForm
-    form_title = 'This is my first form view'
-    message = 'My form submitted'
-
-    def form_get(self, form):
-        form.title.data = 'This was prefilled'
-
-    def form_post(self, form):
-        # post process form
-        flash(self.message, 'info')
-
-
-class MyBooks(BaseView):
-    # route_base = "/mybooks"
-    default_view = 'fetch_all_books'
-
-    @expose('/fetch_all_books')
-    @has_access
-    def fetch_all_books(self):
-        return "Fetched all books"
-
-    @expose('/fetch_book/<int:isbn>')
-    @has_access
-    def fetch_book(self, isbn):
-        return "Fetched book with isbn: " + str(isbn)
-
-    @expose('goodBye/<string:username>')
-    @has_access
-    def good_bye(self, username):
-        msg = "Good Bye " + username + " !"
-        self.update_redirect()
-        return self.render_template('good_bye.html', message=msg)
-
-
-appbuilder.add_view(MyBooks, "All My Books", category='My Books')
-appbuilder.add_link("Fetch a book", href='/mybooks/fetch_book/684531', category='My Books')
-appbuilder.add_link("Good bye", href='/mybooks/goodBye/Bilal', category='My Books')
-appbuilder.add_view(ContactUsFormView, "Contact Us", icon="far fa-question-circle", label=_('Contact Us'),
-                    category="Help", category_icon="far fa-question-circle")
-
-
-"""
-    Create your Model based REST API::
-
-    class MyModelApi(ModelRestApi):
-        datamodel = SQLAInterface(MyModel)
-
-    appbuilder.add_api(MyModelApi)
-
-
-    Create your Views::
-
-
-    class MyModelView(ModelView):
-        datamodel = SQLAInterface(MyModel)
-
-
-    Next, register your Views::
-
-
-    appbuilder.add_view(
-        MyModelView,
-        "My View",
-        icon="fa-folder-open-o",
-        category="My Category",
-        category_icon='fa-envelope'
-    )
-"""
-
-"""
-    Application wide 404 error handler
-"""
-
-
-@appbuilder.app.errorhandler(404)
-def page_not_found(e):
-    return (
-        render_template(
-            "404.html", base_template=appbuilder.base_template, appbuilder=appbuilder
+    show_fieldsets = [
+        (
+            'Main Informations',
+            {'fields': ['book_cover', 'book_title', 'book_summary', 'book_avg_rating', 'publisher']}
         ),
-        404,
-    )
+        (
+            'Details',
+            {'fields': ['book_isbn', 'book_pages', 'book_publication_date', 'book_is_approved'], 'expanded': False}
+        ),
+    ]
+
+
+class PublisherModelView(ModelView):
+    datamodel = SQLAInterface(Publisher)
+    related_views = [BookModelView]
 
 
 db.create_all()
+appbuilder.add_view(
+    BookModelView,
+    "Books List",
+    icon="fas fa-book",
+    category="Manage Books",
+    category_icon="fas fa-book"
+)
+appbuilder.add_view(
+    PublisherModelView,
+    "Publishers List",
+    icon="fas fa-building",
+    category="Manage Books",
+)
